3b4dd2dd297e67178e548157b9fab48288a0f9c5
LUIS <luis@pop-os.localdomain>
2024-03-14 13:03:55 -0500

    new changes

--- realmEmailProvider/context/RealmEmailProvider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren, createContext } from "react";
-const EmailProvider = createContext({});
-function RealmEmailProvider({ children }: PropsWithChildren) {
-  return <EmailProvider.Provider value={{}}>{children}</EmailProvider.Provider>;
-}
-
-export default RealmEmailProvider;
